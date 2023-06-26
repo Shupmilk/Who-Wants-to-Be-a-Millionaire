7d8910037c33eec3548480c33f1a8e21cce29276
andrii <a.shupmilk@gmail.com>
2023-06-26 12:38:38 +0300

    Add redirect to start game after reload page.

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import './index.css';
 const App: React.FC = () => {
 	return (
 		<Routes>
-			<Route path="/who-wants-to-be-a-millionaire" element={<GameStart/>} />
+			<Route path="*" element={<GameStart/>} />
 			<Route path="/who-wants-to-be-a-millionaire/game" element={<Game />} />
 			<Route path="/who-wants-to-be-a-millionaire/game-over" element={<GameOver/>} />
 		</Routes>
