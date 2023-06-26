3e1041038fd4e5d0f994db812817cd7332f5bfd8
andrii <a.shupmilk@gmail.com>
2023-06-26 11:51:36 +0300

    Remove unnecessary Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import {Routes, Route} from "react-router-dom"
-import {Provider} from 'react-redux';
-import store from './redux/store/store';
 import Game from './pages/Game';
 import GameStart from './pages/GameStart';
 import GameOver from './pages/GameOver';
@@ -9,13 +7,11 @@ import './index.css';
 
 const App: React.FC = () => {
 	return (
-		<Provider store={store}>
-			<Routes>
-				<Route path="/" element={<GameStart/>} />
-				<Route path="/game" element={<Game />} />
-				<Route path="/game-over" element={<GameOver/>} />
-			</Routes>
-		</Provider>
+		<Routes>
+			<Route path="/" element={<GameStart/>} />
+			<Route path="/game" element={<Game />} />
+			<Route path="/game-over" element={<GameOver/>} />
+		</Routes>
 	);
 };
 
